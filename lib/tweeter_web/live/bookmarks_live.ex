777984fb8a4b1aba83af10cd5a7191dd85490f1f
Horus Lugo <horusgoul@gmail.com>
2020-12-07 04:51:51 +0100

    feat: pass current_user to the topbar in explore and bookmarks

--- lib/tweeter_web/live/bookmarks_live.ex
+++ lib/tweeter_web/live/bookmarks_live.ex
@@ -2,6 +2,11 @@ defmodule TweeterWeb.BookmarksLive do
   use TweeterWeb, :live_view
 
   @impl true
+  def mount(_args, session, socket) do
+    {:ok, assign_defaults(socket, session)}
+  end
+
+  @impl true
   def handle_params(_, _, socket) do
     {:noreply, socket}
   end
